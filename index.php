e5f8a591d2918c294086052a48032076b8007702
Claudio Rojas <claudio.dcv@gmail.com>
2016-04-16 13:09:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,24 @@
   <title>Prueba de PHP</title>
  </head>
  <body>
- <?php 
- $db = new PDO('pgsql:dbname=d61aoijiddtf6q;host=localhost;port=5432user=ctkljaerhyndbi;password=yneO-NfDHuvhzZQ7UrXa_xBdu3');
+ <?php
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+  // we will fill this out next
+}
+ 
+# Establish db connection
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+    echo "Database connection error."
+    exit;
+}
+ 
+$result = pg_query($db, "SELECT statement goes here");
+?>
+<?php 
+ //$db = new PDO('pgsql:dbname=d61aoijiddtf6q;host=localhost;port=5432user=ctkljaerhyndbi;password=yneO-NfDHuvhzZQ7UrXa_xBdu3');
  
  echo '<p>Hola Mundo</p>'; 
  ?>
