4c7b9c5d21666d84369a5f9ce7d713141a1f0ff9
Claudio Rojas <claudio.dcv@gmail.com>
2016-04-16 13:04:58 -0300

    dpo conect postgre

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
   <title>Prueba de PHP</title>
  </head>
  <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
+ <?php 
+ $db = new PDO('pgsql:dbname=mydb;host=localhost;user=myuser;password=mypass');
+ 
+ echo '<p>Hola Mundo</p>'; 
+ ?>
  </body>
 </html>
